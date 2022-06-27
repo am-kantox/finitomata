195b4c5c30beaaaa9004a5acab85ffc6c15a37a4
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2022-06-27 17:04:17 +0200

    Better docs

--- lib/finitomata/transition.ex
+++ lib/finitomata/transition.ex
@@ -1,9 +1,15 @@
 defmodule Finitomata.Transition do
-  @moduledoc false
+  @moduledoc """
+  The internal representation of `Transition`.
+
+  It includes `from` and `to` states, and the `event`, all represented as atoms.
+  """
 
   alias Finitomata.Transition
 
+  @typedoc "The state of FSM"
   @type state :: atom()
+  @typedoc "The event in FSM"
   @type event :: atom()
 
   @typedoc """
@@ -31,7 +37,10 @@ defmodule Finitomata.Transition do
   end
 
   @doc ~S"""
-      iex> {:ok, transitions} = Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
+      Returns the state _after_ starting one, so-called `entry` state.
+
+      iex> {:ok, transitions} =
+      ...>   Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
       ...> Finitomata.Transition.entry(transitions)
       :s1
   """
@@ -42,7 +51,10 @@ defmodule Finitomata.Transition do
   end
 
   @doc ~S"""
-      iex> {:ok, transitions} = Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
+      Returns `true` if the transition `from` → `to` is allowed, `false` otherwise.
+
+      iex> {:ok, transitions} =
+      ...>   Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
       ...> Finitomata.Transition.allowed?(transitions, :s1, :s2)
       true
       ...> Finitomata.Transition.allowed?(transitions, :s1, :*)
@@ -54,7 +66,10 @@ defmodule Finitomata.Transition do
   end
 
   @doc ~S"""
-      iex> {:ok, transitions} = Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
+      Returns `true` if the state `from` hsa an outgoing transition with `event`, false otherwise.
+
+      iex> {:ok, transitions} =
+      ...>   Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
       ...> Finitomata.Transition.responds?(transitions, :s1, :ok)
       true
       ...> Finitomata.Transition.responds?(transitions, :s1, :ko)
@@ -66,19 +81,33 @@ defmodule Finitomata.Transition do
   end
 
   @doc ~S"""
-      iex> {:ok, transitions} = Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
+      Returns the list of all the transitions, matching the `from` state and the `event`.
+
+      Used internally for the validations.
+
+      iex> {:ok, transitions} =
+      ...>   Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
       ...> Finitomata.Transition.allowed(transitions, :s1, :foo)
       [:s2]
       ...> Finitomata.Transition.allowed(transitions, :s1, :*)
       []
   """
-  @spec allowed([Transition.t()], state(), state()) :: [state()]
+  @spec allowed([Transition.t()], state(), nil | event()) :: [state()]
+  def allowed(transitions, from, event \\ nil)
+
+  def allowed(transitions, from, nil) do
+    for %Transition{from: ^from, to: to} <- transitions, do: to
+  end
+
   def allowed(transitions, from, event) do
     for %Transition{from: ^from, to: to, event: ^event} <- transitions, do: to
   end
 
   @doc ~S"""
-      iex> {:ok, transitions} = Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
+      Returns the not ordered list of states, excluding the starting and ending states `:*`.
+
+      iex> {:ok, transitions} =
+      ...>   Finitomata.PlantUML.parse("[*] --> s1 : foo\ns1 --> s2 : ok\ns2 --> [*] : ko")
       ...> Finitomata.Transition.states(transitions)
       [:s1, :s2]
   """
