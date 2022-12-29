48e1b41b21f878e9720e6562ca34f1ce7238d810
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2022-12-29 15:01:08 +0100

    Added test for example to the pipeline

--- examples/ecto_intergation/mix.exs
+++ examples/ecto_intergation/mix.exs
@@ -5,7 +5,7 @@ defmodule EctoIntergation.MixProject do
     [
       app: :ecto_intergation,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       compilers: compilers(Mix.env()),
       aliases: aliases(),
       start_permanent: Mix.env() == :prod,
