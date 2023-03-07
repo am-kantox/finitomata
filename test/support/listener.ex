e12fbd9f2f51e553abbad0b010cb2b2d1f87cf2a
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2023-03-07 08:29:32 +0100

    Mox listener implementation

---
+++ test/support/listener.ex
@@ -0,0 +1,32 @@
+Mox.defmock(Finitomata.Test.Listener.Mox, for: Finitomata.Listener)
+
+defmodule Finitomata.Test.Listener do
+  @moduledoc false
+
+  @fsm """
+  idle --> |start!| started
+  started --> |do!| done
+  """
+
+  use Finitomata, fsm: @fsm, auto_terminate: true, listener: Finitomata.Test.Listener.Mox
+
+  defstruct pid: nil
+
+  @impl Finitomata
+  def on_transition(:idle, :start!, _, %__MODULE__{pid: pid} = state) do
+    send(pid, :on_start!)
+    {:ok, :started, state}
+  end
+
+  @impl Finitomata
+  def on_transition(:started, :do!, _, %__MODULE__{pid: pid} = state) do
+    send(pid, :on_do!)
+    {:ok, :done, state}
+  end
+
+  @impl Finitomata
+  def on_transition(:done, :__end__, _, %__MODULE__{pid: pid} = state) do
+    send(pid, :on_end)
+    {:ok, :*, state}
+  end
+end
