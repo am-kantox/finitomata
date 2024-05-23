f7d81860df03edd24a903290f8ae4443a4ce78f2
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2024-05-23 18:58:35 +0200

    export `assert_state/1`

--- lib/finitomata/test/ex_unit.ex
+++ lib/finitomata/test/ex_unit.ex
@@ -252,7 +252,7 @@ defmodule Finitomata.ExUnit do
   """
   @spec assert_state(state :: Finitomata.Transition.state(), do_block :: [{:do, Macro.t()}]) ::
           any()
-  def assert_state(state, do_block) do
+  def assert_state(state, do_block \\ []) do
     _ = state
     _ = do_block
     raise(%UndefinedFunctionError{module: __MODULE__, function: :assert_state, arity: 2})
