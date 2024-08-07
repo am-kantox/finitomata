83fcd953925b0b06cf543430a9f72c8b88435b95
Aleksei Matiushkin <am@ambment.cat>
2024-08-07 08:35:28 +0200

    Tests

--- config/config.exs
+++ config/config.exs
@@ -10,4 +10,11 @@ level =
 
 config :logger, level: level
 config :logger, :default_handler, level: level
+config :logger, :default_formatter, colors: [info: :magenta]
 config :logger, compile_time_purge_matching: [[level_lower_than: level]]
+
+config :finitomata, :mox_envs, [:test, :finitomata]
+
+if Mix.env() in [:test, :dev, :finitomata] do
+  config :finitomata, :ext_behaviour, Finitomata.Test.WithMocks.ExtBehaviour.Mox
+end
