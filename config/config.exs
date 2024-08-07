7059a5e7843eb497b2b03599b3afe54c8dacda0e
Aleksei Matiushkin <am@ambment.cat>
2024-08-07 08:22:35 +0200

    Log level for tests to pass

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,12 @@
 import Config
 
-config :logger, level: :error
-config :logger, :default_handler, level: :error
-config :logger, compile_time_purge_matching: [[level_lower_than: :error]]
+level =
+  case Mix.env() do
+    :test -> :debug
+    :prod -> :warning
+    :dev -> :error
+  end
+
+config :logger, level: level
+config :logger, :default_handler, level: level
+config :logger, compile_time_purge_matching: [[level_lower_than: level]]
