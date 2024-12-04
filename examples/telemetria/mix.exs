b61470e137d58314bd567d17840b920195392865
Aleksei Matiushkin <am@ambment.cat>
2024-12-04 13:45:50 +0100

    :paperclip: fix mermaid graph drawing

--- examples/telemetria/mix.exs
+++ examples/telemetria/mix.exs
@@ -8,6 +8,7 @@ defmodule FinitomataWithTelemetria.MixProject do
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       compilers: [:finitomata, :telemetria | Mix.compilers()],
+      docs: docs(),
       deps: deps()
     ]
   end
@@ -26,7 +27,43 @@ defmodule FinitomataWithTelemetria.MixProject do
       {:finitomata, path: "../../"},
       {:telemetry, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
-      {:telemetria, "~> 0.22"}
+      {:telemetria, "~> 0.22"},
+
+      # dev/test
+      {:mox, "~> 1.0", only: [:test]},
+      {:ex_doc, ">= 0.0.0", only: [:dev]}
     ]
   end
+
+  defp docs do
+    [
+      before_closing_body_tag: &before_closing_body_tag/1
+    ]
+  end
+
+  defp before_closing_body_tag(:html) do
+    """
+    <script src="https://cdn.jsdelivr.net/npm/mermaid@8.13.3/dist/mermaid.min.js"></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", function () {
+    mermaid.initialize({ startOnLoad: false });
+    let id = 0;
+    for (const codeEl of document.querySelectorAll("pre code.mermaid")) {
+      const preEl = codeEl.parentElement;
+      const graphDefinition = codeEl.textContent;
+      const graphEl = document.createElement("div");
+      const graphId = "mermaid-graph-" + id++;
+      mermaid.render(graphId, graphDefinition, function (svgSource, bindListeners) {
+        graphEl.innerHTML = svgSource;
+        bindListeners && bindListeners(graphEl);
+        preEl.insertAdjacentElement("afterend", graphEl);
+        preEl.remove();
+      });
+    }
+    });
+    </script>
+    """
+  end
+
+  defp before_closing_body_tag(_), do: ""
 end
--- lib/finitomata/parser/mermaid.ex
+++ lib/finitomata/parser/mermaid.ex
@@ -166,19 +166,22 @@ defmodule Finitomata.Mermaid do
   @impl Parser
   def lint(input) when is_binary(input) do
     case parse(input) do
-      {:ok, transitions} -> Enum.map_join(["graph TD" | transitions], "\n    ", &dump/1)
-      {:error, error} -> "‹ERROR› " <> inspect(error)
+      {:ok, transitions} ->
+        ["graph TD" | transitions] |> Enum.reject(&is_nil/1) |> Enum.map_join("\n    ", &dump/1)
+
+      {:error, error} ->
+        "‹ERROR› " <> inspect(error)
     end
   end
 
   @spec dump(Finitomata.Transition.t() | binary()) :: String.t()
   defp dump(text) when is_binary(text), do: text
 
-  defp dump(%Finitomata.Transition{from: :*, to: to, event: event}),
-    do: "◎ --> |#{event}| #{to}"
+  defp dump(%Finitomata.Transition{from: :*}),
+    do: nil
 
-  defp dump(%Finitomata.Transition{from: from, to: :*, event: event}),
-    do: "#{from} --> |#{event}| ◉"
+  defp dump(%Finitomata.Transition{to: :*}),
+    do: nil
 
   defp dump(%Finitomata.Transition{from: from, to: to, event: event}),
     do: "#{from} --> |#{event}| #{to}"
